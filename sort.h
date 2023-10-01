2d70b54ff85dddc2f5fb1968de6a0d8b9464c825
ermias1068 <ermiasbeyene@gmail.com>
2023-10-01 12:04:30 -0700

    updating the scritpt

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef SORT_H_
+#define SORT_H_
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -19,17 +20,19 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Sort Algorithms prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
-#endif
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H_ */
