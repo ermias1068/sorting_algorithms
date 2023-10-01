4cb4ca5c6146823cd94e4d13b8fdb98f7ff7292c
ermias1068 <ermiasbeyene@gmail.com>
2023-10-01 12:06:13 -0700

    updating the scritpt

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,50 @@
-#include "main.h"
+#include "sort.h"
 
 /**
- * bubble_sort - function that sorts an array
- * of integers in ascending order using the
- * Bubble sort algorithm
+ * swap_pos - swaps position of values in array
  *
- * @array: input arrray
+ * @array: array to be changed
+ * @first: first index
+ * @second: second index
+ */
+void swap_pos(int **array, size_t first, size_t second)
+{
+	int holder;
+
+	holder = (*array)[first];
+	(*array)[first] = (*array)[second];
+	(*array)[second] = holder;
+}
+
+/**
+ * bubble_sort - sorting algorithm that sorts in form
+ * of a bubble
+ *
+ * @array: array to be sorted
  * @size: size of the array
- * Return: no return
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
-	int tmp, swap;
+	size_t i, j, flag;
 
-	for (n = size, swap = 1; n > 0 && swap; n--)
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)	/* go through the array */
 	{
-		swap = 0;
-		for (i = 0; (i + 1) < n; i++)
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)	/* loop only the unsorted */
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = tmp;
+				swap_pos(&array, j, j + 1);
 				print_array(array, size);
-				swap = 1;
+				flag = 1;
 			}
 		}
+
+		/* check if no swap occured (meaning array is sorted) */
+		if (!flag)
+			break;
 	}
 }
